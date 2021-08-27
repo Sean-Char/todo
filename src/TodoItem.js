a99e7479cf0ab4818b349f1743b54b1c748d862f
Sean Charoutanh <scharoutanh@gmail.com>
2021-08-27 13:09:35 -0600

    completed app
    change arrow function to regular function. extended react component in app class component. Add logic to handleChange function. remove react strict mode tags.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const TodoItem = ({text, completed}) => {
+function TodoItem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+    
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={completed}/>
-            <p>{text}</p>
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
         </div>
     )
 }
